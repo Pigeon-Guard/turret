253f9213cf6153f684f428679b18c1ec47ec342e
Marco Betschart <email@marco.betschart.name>
2026-06-04 09:04:29 +0200

    Fix pigpiod startup

--- docker-entrypoint.control.sh
+++ docker-entrypoint.control.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 
-pigpiod -l
+rm -f /var/run/pigpio.pid /run/pigpio.pid 2>/dev/null || true
 
-# wait for pigpio to become ready:
+pigpiod -s 10 -t 0 -x -1
+
+# wait for pigpiod to become ready:
 for i in 1 2 3 4 5 6 7 8 9 10; do
   if pigs t >/dev/null 2>&1; then
     break
